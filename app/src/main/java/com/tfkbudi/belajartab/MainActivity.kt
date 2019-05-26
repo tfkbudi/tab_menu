d24d107e52e728e8e052326894e7409ff2906fa1
taufik budi setiawan <tfkbudi@gmail.com>
2019-05-26 10:02:34 +0700

    init

---
+++ app/src/main/java/com/tfkbudi/belajartab/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tfkbudi.belajartab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
