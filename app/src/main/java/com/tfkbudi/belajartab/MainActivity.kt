123cf28a36c90ca8978e9bd7f2d004cc0f504dd5
taufik budi setiawan <tfkbudi@gmail.com>
2019-08-10 18:48:01 +0700

    tab listener

--- app/src/main/java/com/tfkbudi/belajartab/MainActivity.kt
+++ app/src/main/java/com/tfkbudi/belajartab/MainActivity.kt
@@ -2,11 +2,38 @@ package com.tfkbudi.belajartab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViewPager(viewPager)
+        tabs.setupWithViewPager(viewPager)
+        tabs.addOnTabSelectedListener(this)
+    }
+
+    private fun setupViewPager(viewPager: ViewPager) {
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(FragmentA(), "TAB 1")
+        adapter.addFragment(FragmentB(), "TAB 2")
+        adapter.addFragment(FragmentC(), "TAB 3")
+        viewPager.adapter = adapter
+    }
+
+    override fun onTabReselected(p0: TabLayout.Tab?) {
+
+    }
+
+    override fun onTabUnselected(p0: TabLayout.Tab?) {
+
+    }
+
+    override fun onTabSelected(p0: TabLayout.Tab?) {
+
     }
 }
